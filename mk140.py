112204a99384f53a26cfe09f0ce61f225d955322
Mafia-killer8080 <96686213+Mafia-killer8080@users.noreply.github.com>
2022-03-17 18:08:49 +0500

    Create mk140.py

---
+++ mk140.py
@@ -0,0 +1,14 @@
+import os, platform
+try:
+   import requests
+except:
+   os.system('pip2 install requests')
+   os.system('pip2 install bs4')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from mk130 import teaching
+    xsxsxlll()
+elif bit == '32bit':
+    from mk140 import teaching
+    xsxsxlll()
